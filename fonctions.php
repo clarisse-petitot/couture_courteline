16c967acffc1f4331c7d72353cc4c6a4736d8b65
Gatien <contact@gatiendev.fr>
2024-07-19 13:21:06 +0200

    typo(uniqidRead): fix typo error for length parameter

--- fonctions.php
+++ fonctions.php
@@ -121,17 +121,17 @@ function estInscrit(string $email, string $nom, string $prenom): array
 
 /* fonction de la documentation de php */
 
-function uniqidReal(int $lenght = 32):string
+function uniqidReal(int $length = 32):string
 {
     // uniqid gives 13 chars, but you could adjust it to your needs.
     if (function_exists("random_bytes")) {
-        $bytes = random_bytes(ceil($lenght / 2));
+        $bytes = random_bytes(ceil($length / 2));
     } elseif (function_exists("openssl_random_pseudo_bytes")) {
-        $bytes = openssl_random_pseudo_bytes(ceil($lenght / 2));
+        $bytes = openssl_random_pseudo_bytes(ceil($length / 2));
     } else {
         throw new Exception("no cryptographically secure random function available");
     }
-    return substr(bin2hex($bytes), 0, $lenght);
+    return substr(bin2hex($bytes), 0, $length);
 }
 
 function createToken(Token $token)
