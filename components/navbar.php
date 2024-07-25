016c2ae17c767bc64473d39f598a72ddbfc48d18
Clarisse Petitot <clarisse.petitot@gmail.com>
2024-07-25 15:09:55 +0200

    fix(navbar.php): lien vers accueil coté admin

--- components/navbar.php
+++ components/navbar.php
@@ -11,12 +11,12 @@
         </button>
         <div class="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
-                <li>
-                    <a href="index.php?token=<?= $_GET["token"] ?>" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 " aria-current="page">Accueil</a>
-                </li>
                 <?php
                 if ($utilisateur->getRole() == 'admin') {
                 ?>
+                    <li>
+                        <a href="accueil.php?token=<?= $_GET["token"] ?>" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 " aria-current="page">Accueil</a>
+                    </li>
                     <li>
                         <a href="absence.php?token=<?= $_GET["token"] ?>" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Absence</a>
                     </li>
@@ -32,6 +32,9 @@
                 <?php
                 } else {
                 ?>
+                    <li>
+                        <a href="index.php?token=<?= $_GET["token"] ?>" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 " aria-current="page">Accueil</a>
+                    </li>
                     <li>
                         <a href="absences.php?token=<?= $_GET["token"] ?>" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Prochains Cours</a>
                     </li>
