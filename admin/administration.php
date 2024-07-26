2eaa29eab8b4f38dc4c962b29bd38e69f8a255f8
Clarisse Petitot <clarisse.petitot@gmail.com>
2024-07-26 21:50:50 +0200

    feat(administration.php): ajout page administration

---
+++ admin/administration.php
@@ -0,0 +1,131 @@
+<?php
+
+require_once __DIR__ . "/../classes.php";
+require_once __DIR__ . "/../fonctions.php";
+
+if (!isset($_GET["token"])) {
+    http_response_code(403);
+    header("Location: ../connexion.php");
+    exit;
+};
+
+$token = getToken($_GET["token"]);
+
+if (is_null($token)) {
+    http_response_code(403);
+    header("Location: ../connexion.php");
+    exit;
+}
+
+if (!$token->isValide()) {
+    http_response_code(403);
+    header("Location: ../connexion.php");
+    exit;
+}
+
+$utilisateur = $token->getUtilisateur();
+
+$allcours = getAllCours();
+$bouton = "Voir l'appel";
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prochains Cours</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+</head>
+
+<body>
+    <div class="min-h-screen flex flex-col justify-between">
+        <?php
+        require "../components/navbar.php";
+        ?>
+        <div class="py-5 lg:mx-auto lg:max-w-7xl lg:px-8">
+            <div class="relative">
+                <div class="relative w-full px-20">
+                    <ul role="list" class="inline-flex mx-0 grid sm:grid-cols-1 space-x-0 md:grid-cols-2 md:gap-x-10 lg:grid-cols-3 lg:gap-x-10 place-items-center">
+
+                        <li class="w-[350px] p-5">
+                            <a href="form-patron.php">
+                                <div class=" max-w-sm p-8 bg-blue-100 border rounded-lg">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="m7.848 8.25 1.536.887M7.848 8.25a3 3 0 1 1-5.196-3 3 3 0 0 1 5.196 3Zm1.536.887a2.165 2.165 0 0 1 1.083 1.839c.005.351.054.695.14 1.024M9.384 9.137l2.077 1.199M7.848 15.75l1.536-.887m-1.536.887a3 3 0 1 1-5.196 3 3 3 0 0 1 5.196-3Zm1.536-.887a2.165 2.165 0 0 0 1.083-1.838c.005-.352.054-.695.14-1.025m-1.223 2.863 2.077-1.199m0-3.328a4.323 4.323 0 0 1 2.068-1.379l5.325-1.628a4.5 4.5 0 0 1 2.48-.044l.803.215-7.794 4.5m-2.882-1.664A4.33 4.33 0 0 0 10.607 12m3.736 0 7.794 4.5-.802.215a4.5 4.5 0 0 1-2.48-.043l-5.326-1.629a4.324 4.324 0 0 1-2.068-1.379M14.343 12l-2.882 1.664" />
+                                    </svg>
+                                    <h5 class="mt-4 text-xl font-semibold tracking-tight text-gray-900">Ajouter un patron</h5>
+                                </div>
+                            </a>
+                        </li>
+
+                        <li class="w-[350px] p-5">
+                            <a href="form-ajout-utilisateur.php">
+                                <div class=" max-w-sm p-8 bg-blue-100 border rounded-lg">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
+                                    </svg>
+                                    <h5 class="mt-4 text-xl font-semibold tracking-tight text-gray-900">Ajouter un utilisateur</h5>
+                                </div>
+                            </a>
+                        </li>
+
+                        <li class="w-[350px] p-5">
+                            <a href="form-supprime-utilisateur.php">
+                                <div class=" max-w-sm p-8 bg-blue-100 border rounded-lg">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M22 10.5h-6m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM4 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 10.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
+                                    </svg>
+                                    <h5 class="mt-4 text-xl font-semibold tracking-tight text-gray-900">Supprimer un utilisateur</h5>
+                                </div>
+                            </a>
+                        </li>
+
+                        <li class="w-[350px] p-5">
+                            <a href="form-ajout-cours.php">
+                                <div class=" max-w-sm p-8 bg-blue-100 border rounded-lg">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
+                                    </svg>
+                                    <h5 class="mt-4 text-xl font-semibold tracking-tight text-gray-900">Ajouter un cours</h5>
+                                </div>
+                            </a>
+                        </li>
+
+                        <li class="w-[350px] p-5">
+                            <a href="form-supprime-cours.php">
+                                <div class=" max-w-sm p-8 bg-blue-100 border rounded-lg">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
+                                    </svg>
+                                    <h5 class="mt-4 text-xl font-semibold tracking-tight text-gray-900">Supprimer un cours</h5>
+                                </div>
+                            </a>
+                        </li>
+
+                        <li class="w-[350px] p-5">
+                            <a href="form-annee.php">
+                                <div class=" max-w-sm p-8 bg-blue-100 border rounded-lg">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+                                    </svg>
+                                    <h5 class="mt-4 text-xl font-semibold tracking-tight text-gray-900">Renouveller une année</h5>
+                                </div>
+                            </a>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <?php
+        require '../components/footer.php';
+        ?>
+    </div>
+
+</body>
+
+</html>
