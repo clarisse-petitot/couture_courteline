7ad0a17333f3e7db717466f1a0e83a4062b7ad25
Clarisse Petitot <clarisse.petitot@gmail.com>
2024-07-26 14:42:53 +0200

    style filters-patrons

--- components/filters-patrons.php
+++ components/filters-patrons.php
@@ -1,10 +1,27 @@
+<script>
+    function showFilters() {
+        var fSection = document.getElementById("filterSection");
+        if (fSection.classList.contains("hidden")) {
+            fSection.classList.remove("hidden");
+            fSection.classList.add("block");
+        } else {
+            fSection.classList.add("hidden");
+        }
+    }
+
+    function closeFilterSection() {
+        var fSection = document.getElementById("filterSection");
+        fSection.classList.add("hidden");
+    }
+</script>
+
 <div class="2xl:container 2xl:mx-auto px-4 lg:px-20 md:px-6">
 
-    <div class="md:py-6 py-5 ">
+    <div class="md:py-6 py-5">
         <div class="flex justify-between items-center">
-            <h2 class="lg:text-4xl text-3xl lg:leading-9 leading-7 text-gray-800 font-semibold">Les Patrons disponibles</h2>
+            <h2 class="lg:text-4xl text-3xl lg:leading-9 leading-7 text-gray-800 font-semibold">Patrons disponibles</h2>
             <!-- filters Button (md and plus Screen) -->
-            <button onclick="showFilters()" class="sm:flex hidden cursor-pointer h-[48px] w-[120px] rounded-md bg-pink-500 text-white cursor-pointer justify-center items-center">
+            <button onclick="showFilters()" class="sm:flex hidden cursor-pointer h-[48px] w-[120px] rounded-md bg-blue-700 text-white cursor-pointer justify-center items-center">
                 <svg class="mr-2" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M6 12C7.10457 12 8 11.1046 8 10C8 8.89543 7.10457 8 6 8C4.89543 8 4 8.89543 4 10C4 11.1046 4.89543 12 6 12Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                     <path d="M6 4V8" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
@@ -22,7 +39,7 @@
 
         <!-- Filters Button (Small Screen) -->
 
-        <button onclick="showFilters()" class="cursor-pointer mt-6 block sm:hidden py-2 w-full bg-pink-500 flex text-base leading-4 font-normal text-white justify-center items-center">
+        <button onclick="showFilters()" class="cursor-pointer mt-6 block sm:hidden py-2 w-full bg-blue-700 flex text-base leading-4 font-normal text-white justify-center items-center">
             <svg class="mr-2" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M6 12C7.10457 12 8 11.1046 8 10C8 8.89543 7.10457 8 6 8C4.89543 8 4 8.89543 4 10C4 11.1046 4.89543 12 6 12Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                 <path d="M6 4V8" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
@@ -47,7 +64,7 @@
             </svg>
         </div>
 
-        <form action="patrons.php">
+        <form action="rattrapages.php">
             <div>
                 <input type="hidden" value="<?= $_GET["token"] ?>" name='token'>
             </div>
@@ -63,14 +80,14 @@
                             <path d="M17 4V10" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                         </g>
                     </svg>
-                    <p class="lg:text-lg text-base lg:leading-6 leading-5 font-normal ">Catégories</p>
+                    <p class="lg:text-lg text-base lg:leading-6 leading-5 font-normal ">Categorie</p>
                 </div>
                 <div class="flex flex-wrap mt-4 max-w-lg">
                     <?php
                     foreach ($categories as $categorie) {
                     ?>
                         <div class="flex md:justify-center md:items-center items-center justify-start p-2 pr-6">
-                            <input class="w-4 h-4 mr-2 text-pink-500 focus:ring-0" type="checkbox" value="true" id="categorie-<?= $categorie->getIdCategorie() ?>" name="categorie-<?= $categorie->getIdCategorie() ?>" <?php if (in_array($categorie, $filtres["categories"])) { ?>checked<?php } ?> />
+                            <input class="w-4 h-4 mr-2 text-blue-700 focus:ring-0" type="checkbox" value="true" id="categorie-<?= $categorie->getIdCategorie() ?>" name="categorie-<?= $categorie->getIdCategorie() ?>" <?php if (in_array($categorie->getIdCategorie(), $filtres["categorie"])) { ?>checked<?php } ?> />
                             <div class="inline-block">
                                 <div class="flex space-x-6 justify-center items-center">
                                     <label class="mr-2 text-sm leading-3 font-normal text-gray-600" for="categorie-<?= $categorie->getIdCategorie() ?>"><?= $categorie->getNom() ?></label>
@@ -86,18 +103,18 @@
             <!-- Apply Filter Button (Large Screen) -->
 
             <div class="hidden md:block absolute right-0 bottom-0 md:py-5 py-5">
-                <input type="submit" value="Valider" class="h-[48px] w-[120px] rounded-md bg-pink-500 text-white cursor-pointer">
+                <input type="submit" value="Valider" class="h-[48px] w-[120px] rounded-md bg-blue-700 text-white cursor-pointer">
             </div>
             <!-- Reset Filter Button (Table or lower Screen) -->
 
             <div class="block md:hidden w-full mt-5">
-                <a href="/creations.php"><button type="button" class="w-full text-base leading-4 font-normal py-3 px-5 bg-gray-100 text-pink-500 border border-2 border-pink-500">Réinitialiser</button></a>
+                <a href="/patrons.php?token=<?= $_GET["token"] ?>"><button type="button" class="w-full text-base leading-4 font-normal py-3 px-5 bg-gray-100 text-blue-700 border border-2 border-blue-700">Réinitialiser</button></a>
             </div>
 
             <!-- Apply Filter Button (Table or lower Screen) -->
 
             <div class="block md:hidden w-full mt-5">
-                <input type="submit" value="Valider" class="w-full text-base leading-4 font-normal py-3 px-5 text-white bg-pink-500">
+                <input type="submit" value="Valider" class="w-full text-base leading-4 font-normal py-3 px-5 text-white bg-blue-700">
             </div>
 
             <input type="hidden" value="1" name="id_page">
@@ -105,7 +122,7 @@
 
         <!-- Reset Filter Button (Large Screen) -->
         <div class="hidden md:block absolute right-[135px] bottom-0 md:py-5 py-5">
-            <a href="/creations.php"><button type="button" class="h-[48px] w-[110px] border border-2 rounded-md border-pink-400 bg-gray-100 text-pink-500 ">Réinitialiser</button></a>
+            <a href="/patrons.php?token=<?= $_GET["token"] ?>"><button type="button" class="h-[48px] w-[110px] border border-2 rounded-md border-blue-600 bg-gray-100 text-blue-700 ">Réinitialiser</button></a>
         </div>
 
     </div>
@@ -115,19 +132,3 @@
         display: flex;
     }
 </style>
-<script>
-    function showFilters() {
-        var fSection = document.getElementById("filterSection");
-        if (fSection.classList.contains("hidden")) {
-            fSection.classList.remove("hidden");
-            fSection.classList.add("block");
-        } else {
-            fSection.classList.add("hidden");
-        }
-    }
-
-    function closeFilterSection() {
-        var fSection = document.getElementById("filterSection");
-        fSection.classList.add("hidden");
-    }
-</script>
