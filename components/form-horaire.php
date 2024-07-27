956a6847c71e221ede2ad702491bec062656db6d
Clarisse Petitot <clarisse.petitot@gmail.com>
2024-07-27 19:32:28 +0200

    modif couleur fond sur form

--- components/form-horaire.php
+++ components/form-horaire.php
@@ -5,7 +5,7 @@ $horaires = getAllHoraires();
 <section>
     <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0 w-[400px]">
         <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
-            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <div class="p-6 space-y-4 md:space-y-6 sm:p-8 <?php if($page=='absence' || $page=='rattrapage'){echo 'bg-blue-100 rounded-lg';}?>">
                 <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
                     <?= $titre ?>
                 </h1>
