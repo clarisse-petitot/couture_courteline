df16e7f404bc3f05dc97053d90e2e33f4f3db4d4
Clarisse Petitot <clarisse.petitot@gmail.com>
2024-07-27 20:05:18 +0200

    feat(ajout-utilisateur): possibilité de rajouter des utilisateurs

---
+++ admin/form-ajout-utilisateur.php
@@ -0,0 +1,99 @@
+<?php
+
+require_once __DIR__ . "/../classes.php";
+require_once __DIR__ . "/../fonctions.php";
+
+if (!isset($_GET["token"])) {
+    http_response_code(403);
+    header("Location: ../connexion.php");
+    exit;
+};
+
+$token = getToken($_GET["token"]);
+
+if (is_null($token)) {
+    http_response_code(403);
+    header("Location: ../connexion.php");
+    exit;
+}
+
+if (!$token->isValide()) {
+    http_response_code(403);
+    header("Location: ../connexion.php");
+    exit;
+}
+
+$utilisateur = $token->getUtilisateur();
+
+if (isset($_GET["role"])) {
+    $id_page = 2;
+    $url = "/admin/form-ajout-utilisateur.php?token=" . $_GET['token'];
+} else {
+    if (isset($_GET["date"]) && isset($_GET['id_horaire'])) {
+        $id_page = 3;
+        $horaire = getHoraireFromId($_GET["id_horaire"]);
+        $date = getDateTime($_GET["date"], $horaire);
+        createCours($date, $_GET["id_horaire"]);
+    }
+    //Page fin
+    else {
+        $id_page = 1;
+        $requete = "Role de la personne";
+        $url = "/admin/administration.php?token=" . $_GET['token'];
+    }
+}
+
+if (isset($_POST['submit'])) {
+    echo 'enter';
+    if (isset($_POST['email']) && !empty($_POST['email']) && isset($_POST['id_horaire']) && !empty($_POST['id_horaire']) && isset($_POST['nom']) && !empty($_POST['nom']) && isset($_POST['prenom']) && !empty($_POST['prenom'])) {
+        echo 'envoyer';
+        createUtilisateur($_POST['nom'], $_POST['prenom'], $_POST['email'], $_POST['id_horaire'], $_GET['role']);
+    }
+} else {
+    $res = null;
+    $_POST["email"] = null;
+    $_POST["id_horaire"] = null;
+    $_POST["nom"] = null;
+    $_POST["prenom"] = null;
+}
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajout Utilisateur</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.js"></script>
+</head>
+
+<body>
+    <div class="min-h-screen flex flex-col justify-between">
+        <?php
+        require "../components/navbar.php";
+        require "../components/bouton-admin.php";
+        $page = 'form-ajout-utilisateur';
+        $titre = 'Ajouter un utilisateur';
+        ?>
+        <div class="fixed left-0 top-0 flex h-full w-full items-center justify-center bg-black bg-opacity-50 p-10 z-20">
+            <?php
+            if ($id_page == 1) {
+                $bouton = 'Suivant';
+                require "../components/form-role.php";
+            }
+            if ($id_page == 2) {
+                require "../components/form-utilisateur.php";
+            }
+            ?>
+        </div>
+        <?php
+        require '../components/footer.php';
+        ?>
+    </div>
+
+</body>
+
+</html>
---
+++ components/form-utilisateur.php
@@ -0,0 +1,56 @@
+<section>
+    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0 w-[400px]">
+        <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                    <?= $titre ?>
+                </h1>
+                <form class="space-y-4 md:space-y-6" method='POST'>
+                    <div class="sm:col-span-3">
+                        <label for="nom" class="block text-sm font-medium leading-6 text-gray-900">Nom</label>
+                        <div class="mt-2">
+                            <input type="text" name="nom" id="nom" autocomplete="given-name" value="<?= $_POST['nom'] ?>" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                        </div>
+                    </div>
+
+                    <div class="sm:col-span-3">
+                        <label for="prenom" class="block text-sm font-medium leading-6 text-gray-900">Prenom</label>
+                        <div class="mt-2">
+                            <input type="text" name="prenom" id="prenom" value="<?= $_POST['prenom'] ?>" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                        </div>
+                    </div>
+
+                    <div class="sm:col-span-4">
+                        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Adresse Email</label>
+                        <div class="mt-2">
+                            <input id="email" name="email" type="email" value="<?= $_POST['email'] ?>" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                        </div>
+                    </div>
+                    <?php
+                    if ($_GET['role'] == 'user') {
+                        $horaires = getAllHoraires();
+                    ?>
+                        <div>
+                            <label for="id_horaire" class="block mb-2 text-sm font-medium text-gray-900">Cours de la personne</label>
+                            <select id="id_horaire" name='id_horaire' class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                                <?php
+                                foreach ($horaires as $horaire) {
+                                ?>
+                                    <option value="<?= $horaire->getIdHoraire() ?>"><?= $horaire->getJour() . ' ' . $horaire->getHeure() ?></option>
+                                <?php
+                                }
+                                ?>
+                            </select>
+                        </div>
+                    <?php
+                    }
+                    ?>
+                    <div> 
+                        <a href="<?= $url ?>"><button type="button" class="w-full text-blue-700 bg-white hover:bg-gray-100 border border-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Retour</button></a>
+                    </div>
+                    <input type="submit" name="submit" id='submit' value='Valider' class="w-full text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
