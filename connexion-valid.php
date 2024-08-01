a97b05b984d760c4704c9e45f9a753638bb86bf4
Clarisse Petitot <clarisse.petitot@gmail.com>
2024-08-01 15:55:31 +0200

    feat(connexion-valid.php): ajout page validation de l'envoie de mail

---
+++ connexion-valid.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Validation</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+</head>
+
+<body>
+    <div class="min-h-screen flex flex-col justify-center">
+        <?php
+        $message = 'Connexion';
+        $desc = "Le mail vous a bien été envoyé";
+        $retour='';
+        require "components/finish.php";
+        ?>
+    </div>
+</body>
+
+</html>
--- connexion.php
+++ connexion.php
@@ -3,6 +3,35 @@
 require_once "classes.php";
 require_once "fonctions.php";
 
+if (isset($_POST['submit'])) {
+    if (isset($_POST['email']) && !empty($_POST['email']) && isset($_POST['nom']) && !empty($_POST['nom']) && isset($_POST['prenom']) && !empty($_POST['prenom']) && estInscrit($_POST['email'], $_POST['nom'], $_POST['prenom'])[0] == 0) {
+        require_once 'mail/mail_service.php';
+        $utilisateur = estInscrit($_POST['email'], $_POST['nom'], $_POST['prenom'])[1];
+        $token = new Token(
+            uniqidReal(),
+            time(),
+            $utilisateur
+        );
+        createToken($token);
+        sendMail($utilisateur, $token);
+        header("Location: connexion-valid.php");
+        exit;
+    } else {
+        if (estInscrit($_POST['email'], $_POST['nom'], $_POST['prenom'])[0] == 1) {
+            $res = "Votre adresse email ne correspond pas";
+        } else {
+            if (estInscrit($_POST['email'], $_POST['nom'], $_POST['prenom'])[0] == 2) {
+                $res = "Votre nom ou prenom ne correspond pas";
+            }
+        }
+    }
+} else {
+    $res = null;
+    $_POST["email"] = null;
+    $_POST["nom"] = null;
+    $_POST["prenom"] = null;
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -17,35 +46,6 @@ require_once "fonctions.php";
 
 <body>
 
-    <?php
-    if (isset($_POST['submit'])) {
-        if (isset($_POST['email']) && !empty($_POST['email']) && isset($_POST['nom']) && !empty($_POST['nom']) && isset($_POST['prenom']) && !empty($_POST['prenom']) && estInscrit($_POST['email'], $_POST['nom'], $_POST['prenom'])[0] == 0) {
-            require_once 'mail/mail_service.php';
-            $utilisateur = estInscrit($_POST['email'], $_POST['nom'], $_POST['prenom'])[1];
-            $token = new Token( uniqidReal(),
-            time(),
-            $utilisateur
-            );
-            createToken($token);
-            sendMail($utilisateur,$token);
-            $res = "succes";
-        } else {
-            if (estInscrit($_POST['email'], $_POST['nom'], $_POST['prenom'])[0] == 1) {
-                $res = "Votre adresse email ne correspond pas";
-            } else {
-                if (estInscrit($_POST['email'], $_POST['nom'], $_POST['prenom'])[0] == 2) {
-                    $res = "Votre nom ou prenom ne correspond pas";
-                }
-            }
-        }
-    } else {
-        $res = null;
-        $_POST["email"] = null;
-        $_POST["nom"] = null;
-        $_POST["prenom"] = null;
-    }
-    ?>
-
     <section class="bg-gray-50">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
             <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900">
@@ -59,15 +59,15 @@ require_once "fonctions.php";
                     <form class="space-y-4 md:space-y-6" method="POST">
                         <div>
                             <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Email</label>
-                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5" placeholder="jean@gmail.com" value="<?=$_POST["email"]?>">
+                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5" placeholder="jean@gmail.com" value="<?= $_POST["email"] ?>">
                         </div>
                         <div>
                             <label for="nom" class="block mb-2 text-sm font-medium text-gray-900">Nom</label>
-                            <input type="text" name="nom" id="nom" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5" value="<?=$_POST["nom"]?>" placeholder="Dupuis">
+                            <input type="text" name="nom" id="nom" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5" value="<?= $_POST["nom"] ?>" placeholder="Dupuis">
                         </div>
                         <div>
                             <label for="prenom" class="block mb-2 text-sm font-medium text-gray-900">Prenom</label>
-                            <input type="text" name="prenom" id="prenom" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5" value="<?=$_POST["prenom"]?>" placeholder="Jean">
+                            <input type="text" name="prenom" id="prenom" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5" value="<?= $_POST["prenom"] ?>" placeholder="Jean">
                         </div>
                         <?php
                         if ($res == "succes") {
