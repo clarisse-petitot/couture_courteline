9f8255e51f53c016ef36d66914d17e666e61e3d0
Clarisse Petitot <clarisse.petitot@gmail.com>
2024-09-16 18:17:22 +0200

    fix(extraction.php): changement indice des lignes

--- extraction/extraction.php
+++ extraction/extraction.php
@@ -137,10 +137,10 @@ function setUtilisateur($cheminwithemail, $cheminwithcours, $horaires)
 
         $mysqli->close();
         for ($i = 0; $i < count($ligneswithemail); $i++) {
-            if($ligneswithcours[$i][0]==$ligneswithemail[$i][1].' '.$ligneswithemail[$i][2]){
+            if($ligneswithcours[$i][0]==$ligneswithemail[$i][1].' '.$ligneswithemail[$i][4]){
                 $nom = ucfirst(strtolower($ligneswithemail[$i][1]));
-                $prenom = ucfirst(strtolower($ligneswithemail[$i][2]));
-                $email = $ligneswithemail[$i][8];
+                $prenom = ucfirst(strtolower($ligneswithemail[$i][4]));
+                $email = $ligneswithemail[$i][9];
                 $id_horaire = array_search(substr($ligneswithcours[$i][8],5),$horaires)+1;
                 createUtilisateur($nom, $prenom, $email, $id_horaire, 'user');
             }
